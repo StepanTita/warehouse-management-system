5574eec6b4ee98b28889fe3397086b65b01710e3
StepanTita <stepan.tytarenko@nure.ua>
2019-10-14 01:51:45 +0300

    add created, bugs fixed

--- cargos_backend/cargos_main/forms.py
+++ cargos_backend/cargos_main/forms.py
@@ -1,12 +1,16 @@
 from django import forms
+import datetime as dt
+from .models import Cell
 
 
 class NewCargoForm(forms.Form):
     title = forms.CharField(label="Title", max_length=200)
 
-    row = forms.IntegerField(label="Row")
-    elevation = forms.IntegerField(label="Elevation")
-    position = forms.IntegerField(label="Position")
+    cell = forms.ModelChoiceField(queryset=Cell.objects.all(), label="Cell")
+
+    # row = forms.IntegerField(label="Row")
+    # elevation = forms.IntegerField(label="Elevation")
+    # position = forms.IntegerField(label="Position")
 
     height = forms.DecimalField(decimal_places=2, max_digits=9)
     length = forms.DecimalField(decimal_places=2, max_digits=9)
@@ -14,11 +18,23 @@ class NewCargoForm(forms.Form):
 
     description = forms.CharField(label="Description", max_length=500, widget=forms.Textarea)
 
-    title.widget.attrs.update({'class': 'col-md-12'})
-    row.widget.attrs.update({'class': 'col-md-12'})
-    elevation.widget.attrs.update({'class': 'col-md-12'})
-    position.widget.attrs.update({'class': 'col-md-12'})
-    height.widget.attrs.update({'class': 'col-md-12'})
-    length.widget.attrs.update({'class': 'col-md-12'})
-    width.widget.attrs.update({'class': 'col-md-12'})
-    description.widget.attrs.update({'class': 'col-md-12'})
+    date_added = forms.DateTimeField(initial=dt.datetime.now,
+        widget=forms.DateTimeInput(format=("%Y-%m-%d %H:%M:%S %Z"),
+                                   attrs={'class': 'form-control',
+                                          'placeholder': 'Select a date'}))
+    date_dated = forms.DateField(initial=dt.date.today,
+        widget=forms.DateInput(format=('%Y-%m-%d'),
+                               attrs={'class': 'form-control',
+                                      'placeholder': 'Select a date'}))
+
+    title.widget.attrs.update({'class': 'col-md-7'})
+    # row.widget.attrs.update({'class': 'col-md-7'})
+    # elevation.widget.attrs.update({'class': 'col-md-7'})
+    # position.widget.attrs.update({'class': 'col-md-7'})
+    height.widget.attrs.update({'class': 'col-md-7'})
+    length.widget.attrs.update({'class': 'col-md-7'})
+    width.widget.attrs.update({'class': 'col-md-7'})
+    description.widget.attrs.update({'class': 'col-md-7'})
+    cell.widget.attrs.update({'class': 'col-md-7'})
+    # date_added.widget.attrs.update({'class': 'col-md-7'})
+    # date_dated.widget.attrs.update({'class': 'col-md-7'})
--- cargos_backend/cargos_main/views.py
+++ cargos_backend/cargos_main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import NewCargoForm
+from .models import Cargo, Cell
 
 
 # Create your views here.
@@ -9,7 +10,31 @@ def index(request):
 
 def new_cargo(request):
     if request.method == "POST":
-        pass
+        success = True
+        data = request.POST
+
+        # Create Algo to add new Cargo
+        print(data)
+        new_cargo_obj = Cargo()
+        new_cargo_obj.cell = Cell.objects.get(pk=data['cell'])
+        new_cargo_obj.date_added = data['date_added']
+        new_cargo_obj.date_dated = data['date_dated']
+        new_cargo_obj.title = data['title']
+        new_cargo_obj.description = data['description']
+        new_cargo_obj.height = data['height']
+        new_cargo_obj.width = data['width']
+        new_cargo_obj.length = data['length']
+        new_cargo_obj.save()
+
+        # Create Algo to add new Cargo
+
+        if success:
+            form = NewCargoForm()
+            return render(request, 'new_cargo/new_cargo.html', {'form': form.as_table()})
+        else:
+            form = NewCargoForm()
+            # Create error message !!!!!
+            return render(request, 'new_cargo/new_cargo.html', {'form': form.as_table()})
     else:
         form = NewCargoForm()
         return render(request, 'new_cargo/new_cargo.html', {'form': form.as_table()})
@@ -22,8 +47,17 @@ def ret_cargo(request):
         return render(request, 'ret_cargo/ret_cargo.html')
 
 
-def preview_cargos(request):
-    if request.method == "POST":
-        pass
-    else:
-        return render(request, 'preview_cargos/preview_cargos.html')
+# def preview_cargos(request):
+#     print('Hi')
+#     if request.method == "GET":
+#         success = True
+#         data = request.GET
+#
+#         result = Cargo.objects.get(title__contains=data['title'])
+#         print(result)
+#         if success:
+#             return render(request, 'preview_cargos/preview_cargos.html', {'cargo_list': result})
+#         else:
+#             return render(request, 'preview_cargos/preview_cargos.html')
+#     else:
+#         return render(request, 'preview_cargos/preview_cargos.html')
